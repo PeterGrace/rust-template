b2f96a8f152bd3154048eded2fa9443286f2418e
Peter Grace <pete.grace@gmail.com>
2024-12-13 13:26:09 -0500

    setup logging like I like it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use] extern crate tracing;
+
+use tracing_subscriber::EnvFilter;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+
+    if let Err(_) = env::var("RUST_LOG") {
+        env::set_var("RUST_LOG", "info");
+    };
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_file(true)
+        .with_line_number(true)
+        .init();
+
+    info!("Hello, world!");
 }
