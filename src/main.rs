7d49d0439d0089315b0331ff18fdc1d12e3addbe
Peter Grace <pete.grace@gmail.com>
2024-12-13 13:28:56 -0500

    use or_else instead of the set_var

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 #[macro_use] extern crate tracing;
 
 use tracing_subscriber::EnvFilter;
-use std::env;
 fn main() {
 
-    if let Err(_) = env::var("RUST_LOG") {
-        env::set_var("RUST_LOG", "info");
-    };
+    let filter_layer =
+        EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("warn")).unwrap();
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(filter_layer)
         .with_file(true)
         .with_line_number(true)
         .init();
